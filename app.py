123169212e342e7f8b254c19246602169a9713fc
Nick Ross <Nick.Ross2@bankofengland.co.uk>
2024-06-07 19:38:44 +0100

    changed viz to story

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas
 from ipyvizzu import Chart, Data, Config, Style,  DisplayTarget
 from load_data import load_census_data
 from streamlit.components.v1 import html
-from render_chart import render_chart
+from render_chart import *
 
 
 st.title("2021 Census data")
@@ -17,52 +17,40 @@ census_data = load_census_data()
 data = Data()
 data.add_df(census_data)
 
-# initialize chart
-height = 600
+data = Data()
+data.add_df(census_data)
+
+# chart dimensions
 width = 800
-chart = Chart(width=f"{width}px", height=f"{height}px", display=DisplayTarget.MANUAL)
-chart.animate(data)
+height = 600
 
-# render chart
-# chart_html = render_chart(chart, st.session_state.page_counter)
+# content
 
-chart.animate(
-    Data.filter("record.Year != '2021'"),
-    Config.stackedColumn(
-        {
-            "x": "Year",
-            "y": "Population (M)",
-            "title": "Slide 1",
-            "stackedBy": "Sexual Orientation"
-        }
-    )
-)
-
-
-
-html(chart._repr_html_(), width=width, height=height ) 
-
-
-st.write("""
-         This is a test
+st.subheader("""
+         This is an introduction.
          
          """)
 
-    
-# page buttons    
-    
-max_pages = 5
+aggregate_stats_chart(data, width, height)
 
-left, centre, right = st.columns([2,8,2])
 
-with left:
-    if st.button("Previous"):
-        if st.session_state.page_counter != 1:
-            st.session_state.page_counter-=1
+
+
+    
+# # page buttons    
+    
+# max_pages = 5
+
+# left, centre, right = st.columns([2,8,2])
+
+# with left:
+#     if st.button("Previous"):
+#         if st.session_state.page_counter != 1:
+#             st.session_state.page_counter-=1
         
-with right:
-    if st.button("Next"):
-        if st.session_state.page_counter != max_pages:
-            st.session_state.page_counter += 1
-with centre:
-    st.write(f"Page: {st.session_state.page_counter}")
+# with right:
+#     if st.button("Next"):
+#         if st.session_state.page_counter != max_pages:
+#             st.session_state.page_counter += 1
+# with centre:
+#     st.write(f"Page: {st.session_state.page_counter}")
--- render_chart.py
+++ render_chart.py
@@ -1,37 +1,47 @@
 import streamlit as st
 import pandas
 from ipyvizzu import Chart, Data, Config, Style,  DisplayTarget
+from ipyvizzustory import Story, Slide, Step
 from load_data import load_census_data
 from streamlit.components.v1 import html
 
-def render_chart(_chart, page):
+def aggregate_stats_chart(data, width, height):
     
-    if page == 1 :
-        
-        _chart.animate(
+    story = Story(data)
+    story.set_size(width, height)
+
+    slide1 = Slide(
+        Step(
             Data.filter("record.Year != '2021'"),
-            Config.stackedColumn(
-                {
-                    "x": "Year",
+            Config.stackedColumn({"x": "Year",
                     "y": "Population (M)",
                     "title": "Adult (16+) population in England and Wales",
-                    "stackedBy": "Sexual Orientation"
-                }
-            )
+                    "stackedBy": "Sexual Orientation",
+                    'label' : "Sexual Orientation"
+                    })
         )
-        
-    if page == 2 :
-        
-        _chart.animate(
-            Data.filter("record.Year <= '2021'"),
-            Config.stackedColumn(
-                {
-                    "x": "Year",
-                    "y": "Population (M)",
-                    "title": "Adult (16+) population in England and Wales",
-                    "stackedBy": "Sexual Orientation"
-                }
-            )
+    )
+    story.add_slide(slide1)
+
+    slide2 = Slide()
+
+    slide2.add_step(
+        Step(
+            Data.filter("record.Year <= '2021'")
+    ))
+
+    slide2.add_step(
+        Step(
+            Data.filter("record.Year == '2021'"),
+            Config({
+                'y' : 'Sexual Orientation',
+                'x' : 'Population (M)',
+                'coordSystem' : 'polar',
+                'title': '2021 Sexual Orientation Breakdown'
+                    })
         )
-    
-    return _chart._repr_html_()
+    )
+
+    story.add_slide(slide2)
+
+    html(story._repr_html_(), width=width, height=height)
